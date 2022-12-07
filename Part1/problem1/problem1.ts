5def690ad97d263059dc34d5e41813a8a62f061f
Devi Andri Aldi <deviandrialdi@gmail.com>
2022-12-08 00:34:23 +0700

    Problem Solving part 1

--- Part1/problem1/problem1.ts
+++ Part1/problem1/problem1.ts
@@ -1,6 +1,30 @@
 function simpleEquations(a: number, b: number, c: number) {
   // Your code here
-  // test;
+  /*  ------- PERMINTAAN SOAL ------
+  - x + y + z = A // tambah
+  - xyz = B // kali
+  - x^2 + y^2 + z^2 = C // pangkat 2 + pangkat 2 + pangkat 2
+  
+  */
+
+  // --------------------------- PROSES LOOPING --------------------//
+  let max = Math.max(a, b, c),
+    output;
+
+  for (let x = 1; x <= max; x++) {
+    for (let y = x + 1; y <= max; y++) {
+      for (let z = y + 1; y <= max; y++) {
+        if (
+          x + y + z === a &&
+          x * y * z === b &&
+          x ** 2 + y ** 2 + z ** 2 === c
+        ) {
+          output = `${x} ${y} ${z}`;
+        }
+      }
+    }
+  }
+  return output || "no solution";
 }
 
 export default simpleEquations;
--- Part1/problem3/problem3.ts
+++ Part1/problem3/problem3.ts
@@ -1,9 +1,52 @@
 function jajanBoba(uang, callback) {
   // Your code here
+
+  // -------- Bagian Boba -----------
+  // diket :
+  let sisaUang = uang; // penampung baru
+
+  // Boba: Rp. 5.000, estimated time to get boba: 5 second
+  // pernyataan 1
+  setTimeout(() => {
+    let hargaBoba = 5000;
+    sisaUang = uang - hargaBoba;
+    if (sisaUang <= 0) {
+      console.log("Maaf uang kamu belum cukup untuk membeli Boba");
+      console.log("Sisa uang kamu sebesar" + uang);
+      console.log("\n"); // newline
+    }
+    // pernyataan 2
+    else {
+      console.log("kamu jajan boba dengan harga Rp." + hargaBoba);
+      console.log("sisa uang kamu adalah Rp." + sisaUang);
+      console.log("\n"); // newline
+    }
+  }, 5000); // Boba: Rp. 5.000, estimated time to get boba: 5 second
+
+  setTimeout(() => {
+    callback(sisaUang);
+  }, 9000); // Seblak: Rp. 8.000, estimated time to get seblak: 9 second
 }
+
+//---------------------------- Bagian Seblak-------------------------------//
 
 function jajanSeblak(uang) {
   // Your code here
+
+  // penampung baru
+  let hargaSeblak = 8000;
+  let sisaUang = uang - hargaSeblak;
+
+  // Seblak : Rp. 8.000, perkiraan waktu untuk mendapatkan seblak: 9 detik
+  if (sisaUang < 0) {
+    // pernyataan 2
+    console.log("Maaf uang kamu belum cukup untuk membeli boba dan seblak");
+    console.log("Sisa uang kamu adalah Rp." + uang);
+  } else {
+    // pernyataan 2
+    console.log("Kamu jajan seblak dengan harga Rp." + hargaSeblak);
+    console.log("Sisa uang kamu adalah Rp." + sisaUang);
+  }
 }
 
 jajanBoba(20000, jajanSeblak);
